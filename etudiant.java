239f8d34f98049cabb4fe807c87db60369c9fc0f
kawtarsouhail <kawtarsouhail698@gmail.com>
2024-02-22 09:29:58 +0100

    modification sur branch collaborationFeature

---
+++ etudiant.java
@@ -0,0 +1,19 @@
+public class Etudiant {
+
+    private String nom;
+    private String prenom;
+    private int age;
+    private String notes;
+
+    public Etudiant(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+   public  String collaborationFeature()
+   {
+    return " ";
+     
+   }
+
+}
